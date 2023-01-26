11e3518e8fd1a8caedf9eada2fd8362b6a4372fe
ArvinYang <sugky7302@gmail.com>
2023-01-26 12:04:31 +0000

    assert支援自訂訊息

--- src/shared/assert.ts
+++ src/shared/assert.ts
@@ -1,13 +1,13 @@
 export class Assert {
-    static isTrue(condition: unknown): asserts condition {
-        if (condition === false) throw new Error('Assertion is not true.');
+    static isTrue(condition: unknown, message?: string) {
+        if (condition === false) throw new Error(message ?? 'Assertion is not true.');
     }
 
-    static isFalse(condition: unknown): asserts condition {
-        if (condition === true) throw new Error('Assertion is not false.');
+    static isFalse(condition: unknown, message?: string) {
+        if (condition === true) throw new Error(message ?? 'Assertion is not false.');
     }
 
-    static isEqual<T>(a: T, b: T): asserts a is T {
-        if (a !== b) throw new Error('Two conditions are not equal.');
+    static AreEqual<T>(a: T, b: T, message?: string) {
+        if (a !== b) throw new Error(message ?? 'Two conditions are not equal.');
     }
 }
